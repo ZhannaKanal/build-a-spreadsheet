fac0e8ccbc1fb6134a45a6ddde6cd4d47aa67145
Zhanna <kezhanna.03@mail.ru>
2025-12-11 17:18:43 +0500

    task 1

---
+++ script.js
@@ -0,0 +1,6 @@
+//The global window object represents the browser window (or tab). 
+// It has an onload property which allows you to define behavior 
+// when the window has loaded the entire page, including stylesheets and scripts.
+window.onload = () => {
+  const container = document.getElementById("container");
+};
