363b2a887e3e78fb930718ed8252ffa135bd83d5
Zhanna <kezhanna.03@mail.ru>
2025-12-13 01:08:52 +0500

    task 83

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ const applyFunction = (str) => {
   const noHigh = highPrecedence(str);
   const infix = /([\d.]+)([+-])([\d.]+)/;
   const str2 = infixEval(noHigh, infix);
+  const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i; //This expression will look for function calls like sum(1, 4).
 };
 
 const spreadsheetFunctions = {
