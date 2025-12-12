6390d9e785f37b76e98feb9b7e314608b033527e
Zhanna <kezhanna.03@mail.ru>
2025-12-13 01:15:34 +0500

    task 86

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const applyFunction = (str) => {
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i; //This expression will look for function calls like sum(1, 4).
   const toNumberList = (args) => args.split(",").map(parseFloat);
-  const apply = (fn, args) => {};
+  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()];
 };
 
 const spreadsheetFunctions = {
