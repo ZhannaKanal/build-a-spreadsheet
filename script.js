90b281bf750acdb8f281c2482aa9000f9b1350f8
Zhanna <kezhanna.03@mail.ru>
2025-12-13 01:18:52 +0500

    task 88

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ const applyFunction = (str) => {
   const toNumberList = (args) => args.split(",").map(parseFloat);
   const apply = (fn, args) =>
     spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall, () => {});
 };
 
 const spreadsheetFunctions = {
